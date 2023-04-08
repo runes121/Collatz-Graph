e2eeaa6a31834d63f71ffe3092ed027dd0b9d484
runes121 <75335579+runes121@users.noreply.github.com>
2023-04-08 19:31:26 +0100

    Fixed 'loc' issue.

--- main.py
+++ main.py
@@ -4,24 +4,28 @@ import matplotlib.pyplot as plt
 x_val = []  # Get a list ready to store the x values before plotting
 y_val = []  # Get a list ready to store the y values before plotting
 
-startTime: float = time.time()  # Get the time before the application starts - Used to measure how long the calculations took.
+if __name__ == "__main__":
+    startTime: float = time.time()  # Get the time before the application starts - Used to measure how long the calculations took.
 
-for i in range(15):
-    x = i
-    step = 0
-    while x != 1:  # If x == 1, then the number has completed its journey.
-        step = step + 1
-        x_val.append(step)
-        if (x % 2) == 0:
-            x = x / 2
-            y_val.append(x / 2)
-        else:
-            x = (x * 3) + 1
-            y_val.append((x * 3) + 1)
-    plt.plot(x_val, y_val, label=f"{i}", loc="upper right")
-    x_val.clear()
-    y_val.clear()
+    for i in range(1, 5):
+        x = i
+        step = 0
+        while x != 1:  # If x == 1, then the number has completed its journey.
+            print(x)
+            step = step + 1
+            x_val.append(step)
+            if (x % 2) == 0:
+                x = x / 2
+                y_val.append(x / 2)
+            else:
+                x = (x * 3) + 1
+                y_val.append((x * 3) + 1)
 
-print(f"Took {int(time.time()) - int(startTime)} seconds to process")
-plt.legend()
-plt.show()
+        print(f"Finished {i}")
+        plt.plot(x_val, y_val, label=f"{i}")
+        x_val.clear()
+        y_val.clear()
+
+    print(f"Took {int(time.time()) - int(startTime)} seconds to process")
+    plt.legend(loc="upper right")
+    plt.show()
